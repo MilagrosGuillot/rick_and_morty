b4022dd2e4f3a002b39bdb2a0fa23f5f7a512fff
Milagros <miliguillot12@gmail.com>
2023-08-22 21:14:02 -0300

    deploy2

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const FavoriteModel = require("./controllers/models/Favorite");
 const UserModel = require("./controllers/models/User");
 // EJERCICIO 03
@@ -9,7 +9,7 @@ const UserModel = require("./controllers/models/User");
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 const sequelize = new Sequelize(
-   (`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`),
+   (`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`),
    { logging: false, native: false }
 )
 
--- back/src/server.js
+++ back/src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const PORT = 3001;
 const router = require("./routes")
 const morgan = require("morgan")
 const cors = require("cors");
@@ -9,8 +8,19 @@ server.use(express.json())
 server.use(morgan("dev"))
 server.use(cors());
 server.use("/", router)
+require("dotenv").config()
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://rick-and-morty-livid-gamma.vercel.app'); // 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
 })
 
